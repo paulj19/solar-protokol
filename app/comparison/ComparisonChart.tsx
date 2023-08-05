59c8a66136473c916f00b1c4b99fd7cc52e64aac
paul <diljosepaul@gmail.com>
2023-08-05 22:12:11 +0200

    graph area just wokring

--- app/comparison/ComparisonChart.tsx
+++ app/comparison/ComparisonChart.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { PureComponent, useContext, useEffect, useReducer, useRef, useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area, ComposedChart } from 'recharts';
 import { fetchPredictionParams, fetchUserParams } from "@/api/comparisonParam";
 import { getPredictedMonthlyTotalCost, getPredictedMonthlyUsageCost } from "@/utils/EnergyCostCalculator";
 import { getTotalSolarCost } from "@/utils/SolarCostCalculator";
@@ -10,6 +10,7 @@ import { worker } from '@/mocks/browser'
 import styles from './ComparisonChart.module.css'
 import { ExternPredictionParams, PredictionParams, UserSpecificParams } from '@/types/types';
 import Slider from '../components/Slider';
+import { it } from 'node:test';
 
 if (process.env.NODE_ENV === 'development') {
     worker.listen({ onUnhandledRequest: 'bypass' })
@@ -39,9 +40,29 @@ export default function ComparisonChart() {
         }
         getPredictionParams(123);
     }, [])
-
+    let off;
+    let comparisonDataWithRange;
     if (state) {
         comparisonData = getComparisonData({ ...state, externPredictionParams: { inflationRate, priceCurrentAvgKwh, priceEnpalMonthly: state.externPredictionParams.priceEnpalMonthly, priceBase: state.externPredictionParams.priceBase } });
+        let xx = false;
+        comparisonDataWithRange = comparisonData.reduce((acc, item, i, array) => {
+            if (item.Ohne_PV >= item.Mit_Enpal) {
+                if (i >= 1 && xx === false) {
+                    const intersectionResut = intersect(array[i - 1].year, array[i - 1].Mit_Enpal, item.year, item.Mit_Enpal, array[i - 1].year, array[i - 1].Ohne_PV, item.year, item.Ohne_PV);
+                    if (intersectionResut) {
+                        const { x, y }: any = intersectionResut;
+                        const intersection = { year: Math.floor(x), Ohne_PV: y, Mit_Enpal: y, range: [y, y] }
+                        acc = acc.concat(intersection);
+                        xx = true;
+                    }
+                }
+                const range = item.Ohne_PV !== undefined && item.Mit_Enpal !== undefined
+                    ? [item.Ohne_PV - 1, item.Mit_Enpal + 1]
+                    : [];
+                item['range'] = range;
+            }
+            return acc.concat(item);
+        }, []);
     }
 
     return (
@@ -50,8 +71,9 @@ export default function ComparisonChart() {
                 <div className={styles.chartContainer}>
                     <div className={styles.chart}>
                         <ResponsiveContainer className={styles.responseChart}>
-                            <LineChart
-                                data={comparisonData}
+                            {/* <LineChart */}
+                            <ComposedChart
+                                data={comparisonDataWithRange}
                                 margin={{
                                     top: 5,
                                     right: 30,
@@ -64,10 +86,18 @@ export default function ComparisonChart() {
                                 {/* <YAxis ticks={[0, 50, 100, 150, 200, 250, 300, 350]} /> */}
                                 <YAxis ticks={[0, 100, 200, 300, 400, 500, 600, 700]} />
                                 <Tooltip />
+                                <defs>
+                                    <linearGradient id="splitColor">
+                                        <stop offset="1" stopColor="#bff593" stopOpacity={1} />
+                                    </linearGradient>
+                                </defs>
                                 <Legend />
-                                <Line type="monotone" dataKey="Ohne_PV" stroke="#FF0000" activeDot={{ r: 8 }} label={CustomizedLabel} />
-                                <Line type="monotone" dataKey="Mit_Enpal" stroke="#072543" hide={!showSloar} label={CustomizedLabel} />
-                            </LineChart>
+                                <Line type="linear" dataKey="Ohne_PV" stroke="#FF0000" strokeWidth={1.5} activeDot={{ r: 8 }} label={CustomizedLabel} />
+                                <Line type="linear" dataKey="Mit_Enpal" stroke="#072543" strokeWidth={1.5} hide={!showSloar} label={CustomizedLabel} />
+                                <Area type="linear" dataKey="range" fill="url(#splitColor)" hide={!showSloar} legendType='none' tooltipType='none' />
+                                {/* <Area type="monotone" dataKey="Mit_Enpal" stackId="2" stroke="#072543" fill='#fff' hide={!showSloar} /> */}
+                                {/* <Line type="linear" dataKey="Mit_Enpal" stroke="#072543" hide={false} /> */}
+                            </ComposedChart>
                         </ResponsiveContainer>
                     </div>
                     <div className={styles.toggle}>
@@ -85,6 +115,36 @@ export default function ComparisonChart() {
             }
         </>
     );
+}
+function intersect(x1, y1, x2, y2, x3, y3, x4, y4) {
+    // Check if none of the lines are of length 0
+    if ((x1 === x2 && y1 === y2) || (x3 === x4 && y3 === y4)) {
+        return false;
+    }
+
+    const denominator = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+
+    // Lines are parallel
+    if (denominator === 0) {
+        return false;
+    }
+
+    let ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator;
+    let ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator;
+
+    // is the intersection along the segments
+    if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
+        return false;
+    }
+
+    // Return a object with the x and y coordinates of the intersection
+    let x = x1 + ua * (x2 - x1);
+    let y = y1 + ua * (y2 - y1);
+
+    const line1isHigher = y1 > y3;
+    const line1isHigherNext = y2 > y4;
+
+    return { x, y, line1isHigher, line1isHigherNext };
 }
 
 function CustomizedLabel({ x, y, stroke, value }) {
--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -3,7 +3,7 @@ import styles from '@/app/comparison/ComparisonChart.module.css'
 export default function Slider({ ticks, onChangeHandler, defaultValue, label }) {
     return (
         <div>
-            <input type="range" min={ticks?.[0]} max={ticks?.[ticks.length - 1]} defaultValue={defaultValue} step="0.01" list="tickmarks" id="rangeInput" onChange={(e) => onChangeHandler(Number(e.target?.value))} />
+            <input type="range" min={ticks?.[0]} max={ticks?.[ticks.length - 1]} defaultValue={defaultValue} step={(ticks.at(1) - ticks.at(0)).toFixed(2)} list="tickmarks" id="rangeInput" onChange={(e) => onChangeHandler(Number(e.target?.value))} />
             <datalist id="tickmarks">
                 {ticks.map((value, i) => <option key={i}>{value}</option>)}
             </datalist>
