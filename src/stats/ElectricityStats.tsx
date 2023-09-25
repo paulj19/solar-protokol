074dd9d5312efc309c238db45cb20396df2e2c66
paul <diljosepaul@gmail.com>
2023-09-25 16:32:18 +0200

    fix stats styles and alignment

--- src/stats/ElectricityStats.tsx
+++ src/stats/ElectricityStats.tsx
@@ -15,20 +15,46 @@ export function ElectricityStats(params: PredictionParams) {
 function ElectricityCosts(params: PredictionParams) {
     const totalCost = calcElectricityCostMonthly(params);
 
+    // return (
+    //     <div className={styles.costGrid}>
+    //         <div className={styles.costHeading}>{"Ihre Strom kosten:"}</div>
+    //         <div className="flex justify-center pt-6 items-center gap-3">
+    //             <div className="flex flex-col gap-4 border-gray-300 border-2 rounded-md p-3 w-fit">
+    //                 <div className={styles.costElements}>
+    //                     <div className={styles.costLabel}>{"Grundpreis"}</div>
+    //                     <div className={styles.costNumber}>{params.clientParams.basePrice + "€"}</div>
+    //                 </div>
+    //                 <div className="pl-[30%] text-lg">+</div>
+    //                 <div className={styles.costElements}>
+    //                     <div className={styles.costLabel}>{"Stromverbrauch"}</div>
+    //                     <div className={styles.costNumber}>{totalCost - params.clientParams.basePrice + "€"}</div>
+    //                 </div>
+    //             </div>
+    //             <div
+    //                 className="flex justify-center p-5 gap-3 border-gray-300 border-2 rounded-md h-fit items-center">
+    //                 <div>=</div>
+    //                 <div>
+    //                     <div className={styles.costLabel}>{"Summe"}</div>
+    //                     <div className={styles.costNumber}>{totalCost + "€"}</div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
     return (
         <div className={styles.costGrid}>
             <div className={styles.costHeading}>{"Ihre Strom kosten:"}</div>
-            <div className={styles.costSum}>
+            <div className="flex justify-around pt-4">
                 <div className={styles.costElements}>
                     <div className={styles.costLabel}>{"Grundpreis"}</div>
                     <div className={styles.costNumber}>{params.clientParams.basePrice + "€"}</div>
                 </div>
-                <div className={styles.costAddition}>{"+"}</div>
+                <div className="self-center">+</div>
                 <div className={styles.costElements}>
                     <div className={styles.costLabel}>{"Stromverbrauch"}</div>
                     <div className={styles.costNumber}>{totalCost - params.clientParams.basePrice + "€"}</div>
                 </div>
-                <div className={styles.costAddition}>{"="}</div>
+                <div className="self-center">=</div>
                 <div className={styles.costElements}>
                     <div className={styles.costLabel}>{"Summe"}</div>
                     <div className={styles.costNumber}>{totalCost + "€"}</div>
--- src/stats/SolarStats.tsx
+++ src/stats/SolarStats.tsx
@@ -1,10 +1,10 @@
 import styles from '@/src/stats/stats.module.css'
-import { PredictionParams } from '@/types/types';
-import { calcSolarCostMonthly } from '@/utils/ElectricityCostCalculator';
+import {PredictionParams} from '@/types/types';
+import {calcSolarCostMonthly} from '@/utils/ElectricityCostCalculator';
 
 export default function SolarStats(params: PredictionParams) {
     return (
-        <fieldset className={styles.fieldset}>
+        <fieldset className="border-black border-2 rounded-sm">
             <legend className={styles.headingLegend}>Mit Enpal</legend>
             <ElectricityCosts {...params} />
             {/* <div className={styles.costMonthly}>
@@ -17,34 +17,46 @@ export default function SolarStats(params: PredictionParams) {
 }
 
 function ElectricityCosts(params: PredictionParams) {
-    const { rent, residualConsumptionCostMonthly, basePrice, feedInTariffMonthly, solarCost } = calcSolarCostMonthly(params);
+    const {
+        rent,
+        residualConsumptionCostMonthly,
+        basePrice,
+        feedInTariffMonthly,
+        solarCost
+    } = calcSolarCostMonthly(params);
     return (
-        <div className={styles.costGrid}>
+        <div>
             <div className={styles.costHeading}>{"Ihre Neu Strom kosten:"}</div>
-            <div className={styles.costSum}>
-                <div className={styles.costElements}>
-                    <div className={styles.costLabel}>{"Grundpreis"}</div>
-                    <div className={styles.costNumber}>{basePrice + "€"}</div>
+            <div className="flex justify-around pt-6 items-center">
+                <div className="flex flex-col gap-4 border-gray-300 border-2 rounded-md p-3">
+                    <div className={styles.costElements}>
+                        <div className={styles.costLabel}>{"Grundpreis"}</div>
+                        <div className={styles.costNumber}>{basePrice + "€"}</div>
+                    </div>
+                    <div className="pl-[30%] text-lg">+</div>
+                    <div className={styles.costElements}>
+                        <div className={styles.costLabel}>{"Reststromkosten"}</div>
+                        <div className={styles.costNumber}>{residualConsumptionCostMonthly + "€"}</div>
+                    </div>
                 </div>
-                <div className={styles.costAddition}>{"+"}</div>
-                <div className={styles.costElements}>
-                    <div className={styles.costLabel}>{"Reststromkosten"}</div>
-                    <div className={styles.costNumber}>{residualConsumptionCostMonthly + "€"}</div>
+                <div className="flex flex-col gap-4 border-gray-300 border-2 rounded-md p-3">
+                    {/*<div className={styles.costAddition}>{"+"}</div>*/}
+                    <div className={styles.costElements}>
+                        <div className={styles.costLabel}>{"Enpal Miete"}</div>
+                        <div className={styles.costNumber}>{rent + "€"}</div>
+                    </div>
+                    <div className="pl-[30%] text-lg">-</div>
+                    <div className={styles.costElements}>
+                        <div className={styles.costLabel}>{"Einspeisevergütung"}</div>
+                        <div className={styles.costNumber}>{feedInTariffMonthly + "€"}</div>
+                    </div>
                 </div>
-                <div className={styles.costAddition}>{"+"}</div>
-                <div className={styles.costElements}>
-                    <div className={styles.costLabel}>{"Enpal Miete"}</div>
-                    <div className={styles.costNumber}>{rent + "€"}</div>
-                </div>
-                <div className={styles.costAddition}>{"-"}</div>
-                <div className={styles.costElements}>
-                    <div className={styles.costLabel}>{"Einspeisevergütung"}</div>
-                    <div className={styles.costNumber}>{feedInTariffMonthly + "€"}</div>
-                </div>
-                <div className={styles.costAddition}>{"="}</div>
-                <div className={styles.costElements}>
-                    <div className={styles.costLabel}>{"Summe"}</div>
-                    <div className={styles.costNumber}>{solarCost + "€"}</div>
+                <div className="flex justify-center p-5 gap-3 border-gray-300 border-2 rounded-md h-fit items-center">
+                    <div>=</div>
+                    <div>
+                        <div className={styles.costLabel}>{"Summe"}</div>
+                        <div className={styles.costNumber}>{solarCost + "€"}</div>
+                    </div>
                 </div>
             </div>
         </div>
