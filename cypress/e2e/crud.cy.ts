93ad8245a677b76de1e6c532216ae9d6664e6df3
paul <diljosepaul@gmail.com>
2023-09-28 20:21:17 +0200

    build config changes

---
+++ cypress/e2e/crud.cy.ts
@@ -0,0 +1,220 @@
+import {selector} from "postcss-selector-parser";
+import {screen} from "@testing-library/react";
+import {be} from "date-fns/locale";
+
+function assertListIsEmpty() {
+    cy.get('table tbody tr').should('have.length', 0);
+    cy.get(`[data-testid="no-client-msg"]`).should('be.visible');
+}
+
+function deleteClient() {
+    cy.get('button[aria-label="delete-client"]').click()
+    cy.get('button[aria-label="deleteClient-confirm"]').click()
+    cy.get('[aria-label="deleteClient-snackbar"]').should('be.visible');
+}
+
+function createClient() {
+    cy.get('button[aria-label="add-client"]').click()
+    cy.get('input[name="nickname"]').type('test nickname')
+    cy.get('textarea[name="remarks"]').type('test remarks')
+    // cy.get('input[name="id"]').should('have.value', '1')
+
+    cy.get('textarea[name="basePrice"]').should('not.be.empty')
+    cy.get('textarea[name="unitPrice"]').should('not.be.empty')
+    cy.get('textarea[name="consumptionYearly"]').should('not.be.empty')
+    cy.get('textarea[name="productionYearly"]').should('not.be.empty')
+
+    cy.get('[name="basePrice"]').clear().type('20')
+    cy.get('[name="unitPrice"]').clear().type('40')
+    cy.get('[name="consumptionYearly"]').clear().type('2500')
+    cy.get('[name="productionYearly"]').clear().type('5000')
+
+    cy.get('form').submit()
+    cy.get(`[aria-label="clientCreate-snackbar"]`).should('be.visible');
+
+    cy.get('[aria-label="modal-close"]').click()
+
+    cy.get('table tbody tr').should('have.length', 1)
+    cy.get('table tbody tr td').eq(0).should('contain', '10:00')
+    cy.get('table tbody tr td').eq(2).should('contain', 'test nickname')
+    cy.get('table tbody tr td').eq(3).should('contain', 'test remarks')
+    cy.get('table tbody tr td').eq(4).should('contain', 'open')
+    cy.get('[aria-label="present-client"]').should('be.visible');
+    cy.get('[aria-label="edit-client"]').should('be.visible');
+}
+
+function solarElecChart() {
+    cy.url().then((url) => {
+        expect(url).to.match(/.*?\/solarElecChart\?pDate=\d{4}-\d{2}-\d{2}&clientId=\d+$/);
+    });
+    cy.get('[data-testid="solar-elec-chart"]').should('be.visible')
+    cy.get('[data-testid="solar-toggle"]').should('be.visible')
+}
+function stats() {
+    cy.url().then((url) => {
+        expect(url).to.match(/.*?\/stats\?pDate=\d{4}-\d{2}-\d{2}&clientId=\d+$/);
+    });
+    cy.get('[data-testid="stats"]').should('be.visible')
+}
+
+function generationConsumChart() {
+    cy.url().then((url) => {
+        expect(url).to.match(/.*?\/generationConsumChart\?pDate=\d{4}-\d{2}-\d{2}&clientId=\d+$/);
+    });
+    cy.get('[data-testid="generationConsum-chart"]').should('be.visible')
+}
+
+function goToClientList() {
+    cy.get('[data-testid="right-menu"]').click()
+    cy.get('[aria-label="clientList-item"]').click()
+
+    cy.url().should('eq', Cypress.config().baseUrl + '/')
+}
+function openGeneralParamsEdit() {
+    cy.get('[data-testid="right-menu"]').click()
+    cy.get('[aria-label="generalParams-item"]').click()
+    cy.get(`[data-testid="modal-editGeneralParams"]`).should('be.visible');
+}
+function setDate() {
+    cy.get('[data-testid="clientList-datePicker"]').type('12.12.2023')
+}
+
+describe('CLIENT CRUD', () => {
+
+    it('create client', () => {
+        cy.visit('/')
+        assertListIsEmpty()
+        setDate()
+        createClient()
+        deleteClient()
+    })
+    it('edit client', () => {
+        cy.visit('/')
+        assertListIsEmpty()
+        setDate()
+        createClient()
+        cy.get('[aria-label="edit-client"]').click()
+        cy.get('input[name="nickname"]').clear().type('test nickname edited')
+        cy.get('textarea[name="remarks"]').clear().type('test remarks edited')
+
+        cy.get('[name="basePrice"]').clear().type('25')
+        cy.get('[name="unitPrice"]').clear().type('50')
+        cy.get('[name="consumptionYearly"]').clear().type('3500')
+        cy.get('[name="productionYearly"]').clear().type('6000')
+
+        cy.get('form').submit()
+        cy.get(`[aria-label="clientCreate-snackbar"]`).should('be.visible');
+
+        cy.get('[aria-label="modal-close"]').click()
+        cy.reload()
+        setDate()
+
+        cy.get('[aria-label="edit-client"]').click()
+        cy.get('input[name="nickname"]').should('have.value','test nickname edited')
+        cy.get('textarea[name="remarks"]').should('have.value','test remarks edited')
+
+        cy.get('[name="basePrice"]').should('have.value', 25);
+        cy.get('[name="unitPrice"]').should('have.value', 50);
+        cy.get('[name="consumptionYearly"]').should('have.value', 3500);
+        cy.get('[name="productionYearly"]').should('have.value', 6000);
+
+        cy.get('form').submit()
+        cy.get(`[aria-label="clientCreate-snackbar"]`).should('be.visible');
+
+        cy.get('[aria-label="modal-close"]').click()
+
+        deleteClient()
+    })
+
+    it('present client', () => {
+        cy.visit('/')
+        assertListIsEmpty()
+        createClient()
+        cy.get('[aria-label="present-client"]').click()
+        solarElecChart()
+        cy.get('[data-testid="forward-fab"]').click()
+        stats()
+        cy.get('[data-testid="forward-fab"]').click()
+        generationConsumChart()
+        cy.get('[data-testid="end-fab"]').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/')
+        cy.get('table tbody tr td').eq(4).should('contain', 'completed')
+        deleteClient()
+    })
+
+    it('traverse back and forth', () => {
+        cy.visit('/')
+        assertListIsEmpty()
+        createClient()
+
+        cy.get('[aria-label="present-client"]').click()
+        solarElecChart()
+        cy.get('[data-testid="forward-fab"]').click()
+        stats()
+        cy.get('[data-testid="forward-fab"]').click()
+        generationConsumChart()
+        cy.get('[data-testid="backward-fab"]').click()
+        stats()
+        cy.get('[data-testid="backward-fab"]').click()
+        solarElecChart()
+
+        goToClientList()
+        deleteClient()
+    })
+    it('edit generalParams', () => {
+        cy.visit('/')
+        assertListIsEmpty()
+        createClient()
+
+        cy.get('[aria-label="present-client"]').click()
+        solarElecChart()
+        cy.get('[data-testid="forward-fab"]').click()
+        stats()
+
+        openGeneralParamsEdit()
+
+        cy.get('textarea[name="feedInPrice"]').should('not.be.empty')
+        cy.get('textarea[name="rent"]').should('not.be.empty')
+        cy.get('textarea[name="inflationRate"]').should('not.be.empty')
+        cy.get('textarea[name="electricityIncreaseRate"]').should('not.be.empty')
+        cy.get('textarea[name="rentDiscountRate"]').should('not.be.empty')
+        cy.get('textarea[name="rentDiscountPeriod"]').should('not.be.empty')
+        cy.get('textarea[name="yearLimit"]').should('not.be.empty')
+
+        cy.get('[name="feedInPrice"]').clear().type('9')
+        cy.get('[name="rent"]').clear().type('140')
+        cy.get('[name="inflationRate"]').clear().type('4')
+        cy.get('[name="electricityIncreaseRate"]').clear().type('2')
+        cy.get('[name="rentDiscountRate"]').clear().type('12')
+        cy.get('[name="rentDiscountPeriod"]').clear().type('3')
+        cy.get('[name="yearLimit"]').clear().type('25')
+
+        cy.get('form').submit()
+        cy.get('[aria-label="modalClose-generalParamsEdit"]').click()
+        cy.reload()
+
+        openGeneralParamsEdit()
+
+        cy.get('[name="feedInPrice"]').should('have.value', 9)
+        cy.get('[name="rent"]').should('have.value', 140)
+        cy.get('[name="inflationRate"]').should('have.value', 4)
+        cy.get('[name="electricityIncreaseRate"]').should('have.value', 2)
+        cy.get('[name="rentDiscountRate"]').should('have.value', 12)
+        cy.get('[name="rentDiscountPeriod"]').should('have.value', 3)
+        cy.get('[name="yearLimit"]').should('have.value', 25)
+
+        cy.get('[aria-label="modalClose-generalParamsEdit"]').click()
+        goToClientList()
+        deleteClient()
+    })
+
+    afterEach(() => {
+        // request to set url to null
+
+        // cy.get('delete-client').should('exist').then(() => {
+        //     cy.get('button[aria-label="delete-client"]').click()
+        //     cy.get('button[aria-label="deleteClient-confirm"]').click()
+        //     cy.get('[aria-label="deleteClient-snackbar"]').should('be.visible');
+        // });
+    })
+})
--- next.config.js
+++ next.config.js
@@ -10,4 +10,14 @@ module.exports = {
             },
         ];
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
 };
