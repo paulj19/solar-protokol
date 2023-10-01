119c15a6153db2ec72acd9779372bb5614f32c15
paul <diljosepaul@gmail.com>
2023-10-01 17:22:39 +0200

    wip

--- src/SolarElecChart/SolarElecChart.tsx
+++ src/SolarElecChart/SolarElecChart.tsx
@@ -47,8 +47,9 @@ export default function SolarElecChart() {
         return <Loading/>;
     }
     //todo why undef rendered twice
-    // if (isClientParamError || isGeneralParamsError) {
-        if (true) {
+    if (isClientParamError || isGeneralParamsError) {
+        console.error("error screen")
+        console.trace()
         return <ErrorScreen/>
     }
     //todo no direct url calls with cid, then have to handle loading and error conditions of query
