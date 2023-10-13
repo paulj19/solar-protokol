b721a44799c94e57ac531fcce7e2410c8c639ad0
paul <diljosepaul@gmail.com>
2023-10-13 18:11:37 +0200

    stats design change

--- src/stats/StatsChart.tsx
+++ src/stats/StatsChart.tsx
@@ -1,29 +1,40 @@
 import styles from '@/src/stats/stats.module.css'
-import { PredictionParams } from '@/types/types';
-import { calcElectricityCostMonthly, calcSolarCostMonthly } from '@/utils/ElectricityCostCalculator';
-import { XAxis, BarChart, Bar, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LabelList } from 'recharts';
+import {PredictionParams} from '@/types/types';
+import {calcElectricityCostMonthly, calcSolarCostMonthly} from '@/utils/ElectricityCostCalculator';
+import {
+    XAxis,
+    BarChart,
+    Bar,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+    LabelList,
+    Label, Text
+} from 'recharts';
 
 const renderCustomizedLabel = (props) => {
-    const { x, y, width, height, value } = props;
+    const {x, y, width, height, value} = props;
     const radius = 10;
 
     return (
         <g>
-            <text x={x + width / 2} y={y - radius} fill="#000" textAnchor="middle" style={{ fontWeight: 500 }}>
+            <text x={x + width / 2} y={y - radius} fill="#033142" textAnchor="middle" className="font-serif text-lg font-md">
                 {value + "€"}
             </text>
         </g>
     );
 };
-export default function ComparisonstatsChart(params: PredictionParams) {
+export default function StatsChart(params: PredictionParams) {
     const electricityCost = calcElectricityCostMonthly(params);
-    const { solarCost } = calcSolarCostMonthly(params);
+    const {solarCost} = calcSolarCostMonthly(params);
 
     return (
-        <div className={styles.statsChart} data-testid="comparisonStats-chart">
-            <ResponsiveContainer >
+        <div className="min-w-[400px]" data-testid="comparisonStats-chart">
+            <ResponsiveContainer>
                 <BarChart
-                    data={[{ energyCost: electricityCost, solarCost }]}
+                    data={[{energyCost: electricityCost, solarCost}]}
                     margin={{
                         top: 5,
                         right: 30,
@@ -32,12 +43,16 @@ export default function ComparisonstatsChart(params: PredictionParams) {
                     }}
                 >
                     {/* <CartesianGrid strokeDasharray="3 3" /> */}
-                    <XAxis ticks={["Strom", "Solar"]} />
+                    <XAxis ticks={["Strom", "Solar"]}/>
                     {/* <YAxis ticks={[0, 50, 100, 150, 200, 250, 300, 350]} /> */}
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="energyCost" name={"ohne enpal"} fill="#7ad3ff" label={renderCustomizedLabel} />
-                    <Bar dataKey="solarCost" name={"mit enpal"} fill="#82ca9d" label={renderCustomizedLabel} />
+                    <Tooltip/>
+                    <Legend/>
+                    <Bar dataKey="energyCost" name={"ohne enpal"} fill="#FFA67A" label={renderCustomizedLabel}>
+                        <LabelList position="middle" fill="#fff" className="font-sans">Alte Stromkosten</LabelList>
+                    </Bar>
+                    <Bar dataKey="solarCost" name={"mit enpal"} fill="#00C49F" label={renderCustomizedLabel}>
+                        <LabelList position="middle" fill="#fff" className="font-sans">Neu Stromkosten</LabelList>
+                    </Bar>
                 </BarChart>
             </ResponsiveContainer>
         </div>
--- src/stats/TotalCostSavings.tsx
+++ src/stats/TotalCostSavings.tsx
@@ -6,22 +6,24 @@ export default function TotalCostSavings(params: PredictionParams) {
     const {totalSaved, totalElecCost, totalSolarCost} = calcTotalSaved(params);
 
     return (
-        <div className={styles.totalCostSavings}>
-            <span style={{ fontWeight: 300, fontSize: "x-large", margin: "0 auto" }}>Sie Sparen</span>
-            <EuroCurrencyFormat value={totalSaved} />
-        </div >
+        <>
+            <span className="font-sans font-normal text-2xl tracking-wide text-cyan-900 m-auto p-2">Sie Sparen</span>
+            <EuroCurrencyFormat totalSaved={totalSaved} />
+        </ >
     );
 }
 
-function EuroCurrencyFormat(props) {
-    const { value } = props;
+function EuroCurrencyFormat({totalSaved}) {
+    if(totalSaved < 0) {
+        return <span className={styles.savedAmount}>0€</span>;
+    }
 
     // Format the number as Euro currency with German formatting
     const formattedValue = new Intl.NumberFormat('de-DE', {
         style: 'currency',
         currency: 'EUR',
         maximumFractionDigits: 0,
-    }).format(value);
+    }).format(totalSaved);
 
     return <span className={styles.savedAmount}>{formattedValue}</span>;
 }
